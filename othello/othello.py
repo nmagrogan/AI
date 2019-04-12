9e104323aa3413b4dad5ac45744d228c677e2c43
nmagrogan <nmagrogan@zagmail.gonzaga.edu>
2019-04-12 09:15:28 -0700

    working on othello, trying generate next moves, then check if move is one of them

--- othello/othello.py
+++ othello/othello.py
@@ -26,6 +26,8 @@ class Othello:
                       ["6","0","0","0","0","0","0","0","0"],
                       ["7","0","0","0","0","0","0","0","0"],
                       ["8","0","0","0","0","0","0","0","0"],]
+        #score where 0 is score for black and 1 is score for white
+        self.score = [2,2]
 
     #displays the board
     def display_board(self):
@@ -45,22 +47,40 @@ class Othello:
 
         return letter_pos, number_pos
 
+    #returns all adjacent tiles given a positon
+    def get_adj_tiles(self,letter_pos,number_pos):
+        letter_pos_int = VALID_COLUMN.index(letter_pos)+1
+        adjacent_tiles = [self.board[number_pos+1][letter_pos_int], #above
+                          self.board[number_pos-1][letter_pos_int], #below
+                          self.board[number_pos][letter_pos_int+1], #right
+                          self.board[number_pos][letter_pos_int-1]] #left
+        return adjacent_tiles
+
+
+    def generate_legal_moves(self,letter_pos,number_pos,player_name):
+        next_moves = [('E',3)]
+
+        return next_moves
+
     #checks to see if a valid position was givent for a new tile placement
     #will request new input untill a valid position is given
     def check_pos(self,letter_pos,number_pos,player_name):
-
-        #check if a tile is already in place
-        while self.board[number_pos][VALID_COLUMN.index(letter_pos)+1] == "W" or self.board[number_pos][VALID_COLUMN.index(letter_pos)+1] == "B":
-            print "Tile already inplace, try agian"
+        #generate all of next possible moves, check if new move is one of them
+        next_moves = self.generate_legal_moves(letter_pos,number_pos,player_name)
+        move = (letter_pos_int,number_pos)
+        while move not in next_moves:
+            print "Invalid move, try agian"
             letter_pos,number_pos = self.input_pos(player_name)
+            move = (letter_pos,number_pos)
 
-        #check if a tile is placed in a legal  location
-            #adjacent to a different color with the same color
-            #with another of the same color on the other side
-            #of the adjacent one
+        exit()
 
 
         return letter_pos,number_pos
+
+    #flips tiles based newest move
+    def flip_tiles(self,letter_pos,number_pos):
+        print "flip"
 
 
     #runs through a single turn for a player
@@ -71,6 +91,13 @@ class Othello:
         #checks if initial input was in a valid position, if not it wil
         #request a new input till a valid one is given
         letter_pos,number_pos = self.check_pos(letter_pos,number_pos,player_name)
+        if player_name == "B":
+            self.score[0] = self.score[0]+1
+        else:
+            self.score[1] = self.score[1]+1
+
+        #flips whatever other tiles need to be flipped and changes score
+        self.flip_tiles(letter_pos,number_pos)
 
         #puts tile on board
         self.board[number_pos][VALID_COLUMN.index(letter_pos)+1] = player_name
